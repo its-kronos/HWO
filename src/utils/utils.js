daa99a26bfc01fd073f7d7c4b73d25dcf59e7e55
its-kronos <anseld41@gmail.com>
2024-10-05 12:08:43 -0400

    added point in cone func

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,8 @@
 // Utility functions go here
+import * as THREE from 'three';
 
-function habitZoneRadii(bolMagSun=4.72,apparentMag,d,specType){
-    console.log(specType)
+
+export function habitZoneRadii(bolMagSun=4.72,apparentMag,d,specType){
     var absMag = apparentMag-(5*Math.log(d/10));
     var bolMagStar = absMag+specType;
     var absLum = Math.pow(10,(bolMagStar-bolMagSun)/-2.5);
@@ -10,7 +11,22 @@ function habitZoneRadii(bolMagSun=4.72,apparentMag,d,specType){
 
     return [radInner,radOutter];
 }
+export function pointInCone(loc,coneTip=new THREE.Vector3(0,0,0),coneBase,senSize,focalLen){
 
-function pointInCone(){
-    
+    var h = coneBase.distanceTo(coneTip);
+    var FOV = Math.atan(senSize/(2*focalLen*1000));
+    var r = h*Math.tan(FOV);
+
+    var dir = new THREE.Vector3();
+    dir.subVectors(coneTip,coneBase).normalize();
+    var point = new THREE.Vector3();
+    point.subVectors(loc,coneBase);
+
+    var coneDist = point.dot(dir);
+    var coneRad = (coneDist/h)*r;
+    var orthoDist = point.length() - coneDist;
+    if (orthoDist<=coneRad) {
+        return true
+    }
+    return false
 }
