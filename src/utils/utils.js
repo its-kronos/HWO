7d0a2c1cb489df00cc582444422b557a73d67cc4
its-kronos <140297693+its-kronos@users.noreply.github.com>
2024-10-05 10:58:35 -0400

    Update utils.js semi colons

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,12 +1,12 @@
 // Utility functions go here
 
 function habitZoneRadii(bolMagSun=4.72,apparentMag,d,specType){
-    absMag = apparentMag-(5*Math.log(d/10))
-    bolMagStar = absMag+specType
-    absLum = Math.pow(10,(bolMagStar-bolMagSun)/-2.5)
+    absMag = apparentMag-(5*Math.log(d/10));
+    bolMagStar = absMag+specType;
+    absLum = Math.pow(10,(bolMagStar-bolMagSun)/-2.5);
 
-    radInner = Math.sqrt(absLum/1.1)
-    radOutter = Math.sqrt(absLum/0.54)
+    radInner = Math.sqrt(absLum/1.1);
+    radOutter = Math.sqrt(absLum/0.54);
 
-    return [radInner,radOutter]
+    return [radInner,radOutter];
 }
